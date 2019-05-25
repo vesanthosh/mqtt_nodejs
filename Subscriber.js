c696dc912580ee3bf03a37033f4776ef8c77acf9
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-05-25 17:01:10 +0200

    - formatted code

--- Subscriber.js
+++ Subscriber.js
@@ -1,5 +1,5 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt:192.168.99.100')
+var mqtt = require('mqtt');
+var client  = mqtt.connect('mqtt:192.168.99.100');
  
 client.on('connect', function () {
         client.subscribe('node/js/data');
