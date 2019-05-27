84bf501667b537b02ac7da4330b37fede066fb9a
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-05-27 19:47:00 +0200

    - code optimized

--- Publisher.js
+++ Publisher.js
@@ -3,7 +3,7 @@ var client  = mqtt.connect('mqtt:192.168.99.100')
  
 client.on('connect', function () {
     setInterval(function(){
-        client.publish('node/js/data','test data '+Date.now());
+        client.publish('node/js/data','test data '+Date.now(),{qos:2, retain:true});
         console.log('message sent')
     },3000);
 });
--- Subscriber.js
+++ Subscriber.js
@@ -2,20 +2,25 @@ var mqtt = require('mqtt');
 var MongoClient = require('mongodb').MongoClient;
 var mongoURI = 'mongodb://user:password@192.168.99.100:27017/mqtt_db';
 var client  = mqtt.connect('mqtt:192.168.99.100');
+
+var collection;
  
 client.on('connect', function () {
-        client.subscribe('test/data');
+        client.subscribe('test/data',{qos:1});
+        console.log('Topic subscribed')
+});
+
+MongoClient.connect(mongoURI,{ useNewUrlParser: true },(err,client)=>{
+        if(err) throw err;
+        const db = client.db('mqtt_db');
+        collection = db.collection('mqtt_test_data');
+        console.log('Database connected');
 });
 
 client.on('message',function(topic,message){
 var mqtt_data = message.toString();
-MongoClient.connect(mongoURI,{ useNewUrlParser: true },(err,client)=>{
+collection.insertOne({topic: topic,data: mqtt_data,date: new Date()}, (err, result)=>{
         if(err) throw err;
-        const db = client.db('mqtt_db');
-        const collection = db.collection('mqtt_test_data');
-        collection.insertOne({topic: topic,data: mqtt_data,date: new Date()}, (err, result)=>{
-                if(err) throw err;
-                console.log(result.ops[0]);
-        });
+        console.log(result.ops[0]);
 });
 });
