6fcdefd0c4ef159fa5a9f803aabb39a912d83812
Santhosh Venkatesan <vesanthosh@gmail.com>
2019-05-28 20:48:58 +0200

    - code format

--- Publisher.js
+++ Publisher.js
@@ -1,9 +1,9 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt:192.168.99.100')
- 
+var client = mqtt.connect('mqtt:192.168.99.100')
+
 client.on('connect', function () {
-    setInterval(function(){
-        client.publish('node/js/data','test data '+Date.now(),{qos:2, retain:true});
+    setInterval(function () {
+        client.publish('node/js/data', 'test data ' + Date.now(), { qos: 2, retain: true });
         console.log('message sent')
-    },3000);
+    }, 3000);
 });
--- Subscriber.js
+++ Subscriber.js
@@ -1,26 +1,26 @@
 var mqtt = require('mqtt');
 var MongoClient = require('mongodb').MongoClient;
 var mongoURI = 'mongodb://user:password@192.168.99.100:27017/mqtt_db';
-var client  = mqtt.connect('mqtt:192.168.99.100');
+var client = mqtt.connect('mqtt:192.168.99.100');
 
 var collection;
- 
+
 client.on('connect', function () {
-        client.subscribe('test/data',{qos:1});
+        client.subscribe('test/data', { qos: 1 });
         console.log('Topic subscribed')
 });
 
-MongoClient.connect(mongoURI,{ useNewUrlParser: true },(err,client)=>{
-        if(err) throw err;
+MongoClient.connect(mongoURI, { useNewUrlParser: true }, (err, client) => {
+        if (err) throw err;
         const db = client.db('mqtt_db');
         collection = db.collection('mqtt_test_data');
         console.log('Database connected');
 });
 
-client.on('message',function(topic,message){
-var mqtt_data = message.toString();
-collection.insertOne({topic: topic,data: mqtt_data,date: new Date()}, (err, result)=>{
-        if(err) throw err;
-        console.log(result.ops[0]);
-});
+client.on('message', function (topic, message) {
+        var mqtt_data = message.toString();
+        collection.insertOne({ topic: topic, data: mqtt_data, date: new Date() }, (err, result) => {
+                if (err) throw err;
+                console.log(result.ops[0]);
+        });
 });
